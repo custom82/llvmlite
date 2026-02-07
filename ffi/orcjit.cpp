1a652c0c3d88b743d12899397ad14193ad04d436
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2026-02-07 17:17:31 +0100

    Fix LLVM 21 build breakages in FFI layer

--- ffi/orcjit.cpp
+++ ffi/orcjit.cpp
@@ -82,6 +82,51 @@ LLVMPY_CreateLLJITCompiler(LLVMTargetMachineRef tm, bool suppressErrors,
                 .setFeatures(template_tm->getTargetFeatureString())
                 .setOptions(template_tm->Options));
     }
+#if LLVM_VERSION_MAJOR >= 21
+    builder.setObjectLinkingLayerCreator(
+        [=](llvm::orc::ExecutionSession &session)
+            -> llvm::Expected<std::unique_ptr<llvm::orc::ObjectLayer>> {
+            const auto triple =
+                session.getExecutorProcessControl().getTargetTriple();
+            if (useJitLink) {
+                auto linkingLayer =
+                    std::make_unique<llvm::orc::ObjectLinkingLayer>(session);
+
+                /* FIXME(LLVM16): In newer LLVM versions, there is a simple
+                 * EnableDebugSupport flag on the builder and we don't need to
+                 * do any of this. */
+                //  if (triple.getObjectFormat() == Triple::ELF ||
+                //     triple.getObjectFormat() == Triple::MachO) {
+                //     linkingLayer->addPlugin(
+                //         std::make_unique<orc::GDBJITDebugInfoRegistrationPlugin>(
+                //             ExecutorAddr::fromPtr(
+                //                 &llvm_orc_registerJITLoaderGDBWrapper)));
+                // }
+                if (triple.isOSBinFormatCOFF()) {
+                    linkingLayer->setOverrideObjectFlagsWithResponsibilityFlags(
+                        true);
+                    linkingLayer->setAutoClaimResponsibilityForObjectSymbols(
+                        true);
+                }
+                return linkingLayer;
+            }
+
+            auto linkingLayer =
+                std::make_unique<llvm::orc::RTDyldObjectLinkingLayer>(
+                    session, [](const llvm::MemoryBuffer &) {
+                        return std::make_unique<llvm::SectionMemoryManager>();
+                    });
+            if (triple.isOSBinFormatCOFF()) {
+                linkingLayer->setOverrideObjectFlagsWithResponsibilityFlags(
+                    true);
+                linkingLayer->setAutoClaimResponsibilityForObjectSymbols(true);
+            }
+            linkingLayer->registerJITEventListener(
+                *llvm::JITEventListener::createGDBRegistrationListener());
+
+            return linkingLayer;
+        });
+#else
     builder.setObjectLinkingLayerCreator(
         [=](llvm::orc::ExecutionSession &session, const llvm::Triple &triple)
             -> std::unique_ptr<llvm::orc::ObjectLayer> {
@@ -123,6 +168,7 @@ LLVMPY_CreateLLJITCompiler(LLVMTargetMachineRef tm, bool suppressErrors,
                 return linkingLayer;
             }
         });
+#endif
 
     auto jit = builder.create();
 

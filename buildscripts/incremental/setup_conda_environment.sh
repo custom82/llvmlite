2db6583d80836502d09b8fea1447585019bf7492
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2026-02-07 17:12:19 +0100

    Bump LLVM support baseline and build configs to 21

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -22,12 +22,12 @@ set +v
 source activate $CONDA_ENV
 set -v
 
-# Install llvmdev 20 and compiler toolchain for linux
+# Install llvmdev 21 and compiler toolchain for linux
 # 
 if [[ "$(uname)" == "Linux" ]]; then
-    $CONDA_INSTALL numba/label/dev::llvmdev=20 gcc_linux-64=11 gxx_linux-64=11
+    $CONDA_INSTALL numba/label/dev::llvmdev=21 gcc_linux-64=11 gxx_linux-64=11
 else
-    $CONDA_INSTALL numba/label/dev::llvmdev=20
+    $CONDA_INSTALL numba/label/dev::llvmdev=21
 fi
 
 # Install dependencies for code coverage (codecov.io)
